4c99ec3a008d2056ed3f70bdca012281214f8f10
Mansur Marvanov <mansur.marvanov@linecorp.com>
2021-03-10 09:56:06 +0900

    Update

--- ledblinking.py
+++ ledblinking.py
@@ -2,6 +2,7 @@
 
 import time
 import random
+import os
 
 def initialize(pins):
     GPIO.setmode(GPIO.BCM)
@@ -10,7 +11,7 @@ def initialize(pins):
         GPIO.setup(p,GPIO.OUT)
         GPIO.output(p, False)
 
-def tail_blinking(n, tail, speed):
+def circle_blinking(n, tail, speed):
     state = [False for _ in range(n)]
     while True:
         for i in range(n):
@@ -19,6 +20,48 @@ def tail_blinking(n, tail, speed):
             time.sleep(speed)
             state[i-tail] = False
             yield state
+            time.sleep(0.1 * speed)
+
+def go_and_back(n, tail, speed):
+    state = [False for _ in range(n)]
+    while True:
+        for i in range(n+tail):
+            if i < n:
+                state[i] = True
+            yield state
+            time.sleep(speed)
+            if i-tail >= 0:
+                state[i-tail] = False
+            yield state
+            time.sleep(0.1 * speed)
+        time.sleep(speed)
+        for i in range(n+tail):
+            if i < n:
+                state[-1 - i] = True
+            yield state
+            time.sleep(speed)
+            if i-tail >= 0:
+                state[-1 - (i-tail)] = False
+            yield state
+            time.sleep(0.1 * speed)
+        time.sleep(speed)
+
+
+def go_and_back_2(n, tail, speed):
+
+    snake = go_and_back(n, tail, speed)
+
+    for s in snake:
+        state = [False for _ in range(n)]
+        # import pdb; pdb.set_trace()
+        for i in range(len(state)):
+            # print(i, s[i])
+            if s[i] == True:
+                state[i] = True
+                state[-1-i] = True
+        yield state
+        
+
 
 def apply_state(pins, prev, curr):
     if prev == None:
@@ -29,14 +72,30 @@ def apply_state(pins, prev, curr):
         if prev[i] != curr[i]:
             GPIO.output(pins[i], curr[i])
 
+def console_draw(state):
+    os.write(1, "\r")
+    for s in state:
+        if s:
+            os.write(1, "*")
+        else:
+            os.write(1, " ")
+
 if __name__ == "__main__":
-    import RPi.GPIO as GPIO
+    try:
+        import RPi.GPIO as GPIO
+        gpio = True
+    except ImportError:
+        print("Failed to import RPi.GPIO; emulating only..")
+        gpio = False
 
     pins = [27, 22, 5, 6, 13, 19, 26, 16, 20]
     tail = 2
 
-    initialize(pins)
+    if gpio:
+        initialize(pins)
     
     prev = None
-    for curr in tail_blinking(len(pins), 2, 0.2):
-        apply_state(pins, prev, curr)
+    for curr in go_and_back_2(len(pins), tail, 0.1):
+        if gpio:
+            apply_state(pins, prev, curr)
+        console_draw(curr)
--- ledblinking_test.py
+++ ledblinking_test.py
@@ -3,7 +3,7 @@
 import ledblinking
 
 def test_tail_blinking():
-    g = ledblinking.tail_blinking(4, 2, 0)
+    g = ledblinking.circle_blinking(4, 2, 0)
     assert next(g) == [True, False, False, False]
     assert next(g) == [True, False, False, False]
     assert next(g) == [True, True, False, False]
@@ -16,3 +16,45 @@ def test_tail_blinking():
     assert next(g) == [True, False, False, True]
     assert next(g) == [True, True, False, True]
     assert next(g) == [True, True, False, False]
+
+def test_go_and_back():
+    g = ledblinking.go_and_back(4, 2, 0)
+    # go
+    assert next(g) == [True, False, False, False]
+    assert next(g) == [True, False, False, False]
+    assert next(g) == [True, True, False, False]
+    assert next(g) == [True, True, False, False]
+    assert next(g) == [True, True, True, False]
+    assert next(g) == [False, True, True, False]
+    assert next(g) == [False, True, True, True]
+    assert next(g) == [False, False, True, True]
+    assert next(g) == [False, False, True, True]
+    assert next(g) == [False, False, False, True]
+    assert next(g) == [False, False, False, True]
+    assert next(g) == [False, False, False, False]
+    # back
+    assert next(g) == [False, False, False, True]
+    assert next(g) == [False, False, False, True]
+    assert next(g) == [False, False, True, True]
+    assert next(g) == [False, False, True, True]
+    assert next(g) == [False, True, True, True]
+    assert next(g) == [False, True, True, False]
+    assert next(g) == [True, True, True, False]
+    assert next(g) == [True, True, False, False]
+    assert next(g) == [True, True, False, False]
+    assert next(g) == [True, False, False, False]
+    assert next(g) == [True, False, False, False]
+    assert next(g) == [False, False, False, False]
+
+def test_go_and_back_2():
+    g = ledblinking.go_and_back_2(4, 1, 0)
+    assert next(g) == [True, False, False, True]
+    assert next(g) == [True, False, False, True]
+    assert next(g) == [True, True, True, True]
+    assert next(g) == [False, True, True, False]
+    assert next(g) == [False, True, True, False]
+    assert next(g) == [False, True, True, False]
+    assert next(g) == [True, True, True, True]
+    assert next(g) == [True, False, False, True]
+    assert next(g) == [True, False, False, True]
+    assert next(g) == [False, False, False, False]
